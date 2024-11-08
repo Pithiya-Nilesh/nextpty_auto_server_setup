da96dd48a820a73ed25cbc877a311dddd57fae10
Nilesh-Pithiya-Sanskar <nilesh@sanskartechnolab.com>
2024-11-08 12:44:35 +0530

    init commit with webhooks

---
+++ nextpty_auto_server_setup/nextpty_auto_server_setup/doctype/frappe_cloud_credentials/frappe_cloud_credentials.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, NextPTY and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Frappe Cloud Credentials", {
+// 	refresh(frm) {
+
+// 	},
+// });
