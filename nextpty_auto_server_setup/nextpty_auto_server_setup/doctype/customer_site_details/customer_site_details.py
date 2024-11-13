d6bddce252952fe8e14aa99a7d24b39a29aad2e8
Nilesh-Pithiya-Sanskar <nilesh@sanskartechnolab.com>
2024-11-13 13:15:02 +0530

    change layout and rq job

--- nextpty_auto_server_setup/nextpty_auto_server_setup/doctype/customer_site_details/customer_site_details.py
+++ nextpty_auto_server_setup/nextpty_auto_server_setup/doctype/customer_site_details/customer_site_details.py
@@ -3,23 +3,21 @@
 
 import frappe
 from frappe.model.document import Document
-from nextpty_auto_server_setup.apis.site import create_site_in_frappe_cloud
 
 
 class CustomerSiteDetails(Document):
-	def after_insert(self):
+	def on_update(self):
 		for site in self.site_details:
 			if site.status == "Creation Pending":
 				frappe.enqueue("nextpty_auto_server_setup.apis.site.create_site_in_frappe_cloud", site_name=site.site_name, timeout=1000)
 				site.status = "Pending"
+
+			# if site.status == "":
+			# 	pass
+			# if site.status == "":
+			# 	pass
+			# if site.status == "":
+			# 	pass
+   
 		self.db_update()
 		frappe.db.commit()
-   
-	def on_update(self):
-		for site in self.site_details:
-			if site.status == "":
-				pass
-			if site.status == "":
-				pass
-			if site.status == "":
-				pass
